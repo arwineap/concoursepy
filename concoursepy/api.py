990845978d56f7f071988cff3a0d62500a3fde48
Alex Arwine <alex.arwine@thrivemarket.com>
2018-09-12 16:08:59 -0700

    Adding new routes from upstream
    Rename routes that did not match upstream's name.
    
    Old method names are still there.

--- concoursepy/api.py
+++ concoursepy/api.py
@@ -14,29 +14,83 @@ class api:
     def jobs(self, pipeline_name):
         return self.get("%s/api/v1/teams/%s/pipelines/%s/jobs" % (self.url, self.team, pipeline_name))
 
-    def build_plan(self, build_id):
-        return self.get("%s/api/v1/builds/%s/plan" % (self.url, build_id))
+    def list_builds(self):
+        return self.get("%s/api/v1/builds" % (self.url))
+
+    def get_build(self, build_id):
+        return self.get("%s/api/v1/build/%s" % (self.url, build_id))
 
     def build(self, build_id):
-        return self.get("%s/api/v1/builds/%s" % build_id)
+        """Leaving this here for backwards compatibility."""
+        return self.get_build(build_id)
 
-    def versions(self, pipeline_name, resource_name, limit=100):
+    def get_build_plan(self, build_id):
+        return self.get("%s/api/v1/builds/%s/plan" % (self.url, build_id))
+
+    def build_plan(self, build_id):
+        """Leaving this here for backwards compatibility."""
+        return self.get_build_plan(build_id)
+
+    def send_input_to_build_plan(self, build_id, plan_id):
+        return self.put("%s/api/v1/builds/%s/plan/%s/input" % (self.url, build_id, plan_id))
+
+    def read_output_from_build_plan(self, build_id, plan_id):
+        return self.get("%s/api/v1/builds/%s/plan/%s/input" % (self.url, build_id, plan_id))
+
+    def build_events(self, build_id):
+        return self.get("%s/api/v1/builds/%s/events" % (self.url, build_id))
+
+    def build_resources(self, build_id):
+        return self.get("%s/api/v1/builds/%s/resources" % (self.url, build_id))
+
+    def abort_build(self, build_id):
+        return self.put("%s/api/v1/builds/%s/abort" % (self.url, build_id))
+
+    def get_build_preperation(self, build_id):
+        return self.get("%s/api/v1/builds/%s/preperation" % (self.url, build_id))
+
+    def list_resource_versions(self, pipeline_name, resource_name, limit=100):
         return self.get("%s/api/v1/teams/%s/pipelines/%s/resources/%s/versions?limit=%s" % (self.url, self.team, pipeline_name, resource_name, limit))
 
-    def input_to(self, pipeline_name, resource_name, resource_id):
+    def versions(self, pipeline_name, resource_name, limit=100):
+        """Leaving this here for backwards compatibility."""
+        return self.list_resource_versions(pipeline_name, resource_name, limit)
+
+    def get_resource_version(self, pipeline_name, resource_name, resource_version_id):
+        return self.get("%s/api/v1/teams/%s/pipelines/%s/resources/%s/versions/%s" % (self.url, self.team, pipeline_name, resource_name, resource_version_id))
+
+    def enable_resource_version(self, pipeline_name, resource_name, resource_version_id):
+        return self.put("%s/api/v1/teams/%s/pipelines/%s/resources/%s/versions/%s/enable" % (self.url, self.team, pipeline_name, resource_name, resource_version_id))
+
+    def enable(self, pipeline_name, resource_name, resource_version_id):
+        return self.enable_resource_version(pipeline_name, resource_name, resource_version_id)
+
+    def disable_resource_version(self, pipeline_name, resource_name, resource_version_id):
+        return self.put("%s/api/v1/teams/%s/pipelines/%s/resources/%s/versions/%s/disable" % (self.url, self.team, pipeline_name, resource_name, resource_version_id))
+
+    def disable(self, pipeline_name, resource_name, resource_version_id):
+        return self.disable_resource_version(pipeline_name, resource_name, resource_version_id)
+
+    def list_builds_with_version_as_input(self, pipeline_name, resource_name, resource_id):
         return self.get("%s/api/v1/teams/%s/pipelines/%s/resources/%s/versions/%s/input_to" % (self.url, self.team, pipeline_name, resource_name, resource_id))
 
-    def output_of(self, pipeline_name, resource_name, resource_id):
-        return self.get("%s/api/v1/teams/%s/pipelines/%s/resources/%s/versions/%s/output_of" % (self.url, self.team, pipeline_name, resource_name, resource_id))
+    def input_to(self, pipeline_name, resource_name, resource_id):
+        """Leaving this here for backwards compatibility."""
+        return self.list_builds_with_version_as_input(pipeline_name, resource_name, resource_id)
 
-    def trigger(self, pipeline_name, job_name):
+    def list_builds_with_version_as_output(self, pipeline_name, resource_name, resource_id):
+        return self.get("%s/api/v1/teams/%s/pipelines/%s/resources/%s/versions/%s/output_to" % (self.url, self.team, pipeline_name, resource_name, resource_id))
+
+    def output_of(self, pipeline_name, resource_name, resource_id):
+        """Leaving this here for backwards compatibility."""
+        return self.list_builds_with_version_as_output(pipeline_name, resource_name, resource_id)
+
+    def create_job_build(self, pipeline_name, job_name):
         return self.post("%s/api/v1/teams/%s/pipelines/%s/jobs/%s/builds" % (self.url, self.team, pipeline_name, job_name))
 
-    def enable(self, pipeline_name, resource_name, job_name):
-        return self.put("%s/api/v1/teams/%s/pipelines/%s/resources/%s/versions/%s/enable" % (self.url, self.team, pipeline_name, resource_name, job_name))
-
-    def disable(self, pipeline_name, resource_name, job_name):
-        return self.put("%s/api/v1/teams/%s/pipelines/%s/resources/%s/versions/%s/disable" % (self.url, self.team, pipeline_name, resource_name, job_name))
+    def trigger(self, pipeline_name, job_name):
+        """Leaving this here for backwards compatibility."""
+        return self.create_job_build(pipeline_name, job_name)
 
     def pause_job(self, pipeline_name, job_name):
         return self.put("%s/api/v1/teams/%s/pipelines/%s/jobs/%s/pause" % (self.url, self.team, pipeline_name, job_name))
@@ -53,8 +107,29 @@ class api:
     def get_pipeline(self, pipeline_name):
         return self.get("%s/api/v1/teams/%s/pipelines/%s" % (self.url, self.team, pipeline_name))
 
+    def list_all_jobs(self):
+        return self.get("%s/api/v1/jobs")
+
+    def list_jobs(self, pipeline_name):
+        return self.get("%s/api/v1/teams/%s/pipelines/%s/jobs" % (self.url, self.team, pipeline_name))
+
+    def get_job(self, pipeline_name, job_name):
+        return self.get("%s/api/v1/teams/%s/pipelines/%s/jobs/%s")
+
+    def list_job_builds(self, pipeline_name, job_name):
+        return self.get("%s/api/v1/teams/%s/pipelines/%s/jobs/%s/builds" % (self.url, self.team, pipeline_name, job_name))
+
+    def get_job_build(self, pipeline_name, job_name, build_name):
+        return self.get("%s/api/v1/teams/%s/pipelines/%s/jobs/%s/builds/%s" % (self.url, self.team, pipeline_name, job_name, build_name))
+
+    def job_badge(self, pipeline_name, job_name):
+        return self.get("%s/api/v1/teams/%s/pipelines/%s/jobs/%s/badge" % (self.url, self.team, pipeline_name, job_name))
+
+    def main_job_badge(self, pipeline_name, job_name):
+        return self.get("%s/api/v1/pipelines/%s/jobs/%s/badge" % (self.url, pipeline_name, job_name))
+
     def auth(self):
-        r = requests.get("%s/auth/basic/token?team_name=%s" % (self.url, self.team), auth=(self.username, self.password), headers = {'Content-Type': 'application/json'})
+        r = requests.get("%s/auth/basic/token?team_name=%s" % (self.url, self.team), auth=(self.username, self.password), headers={'Content-Type': 'application/json'})
         if r.status_code == requests.codes.ok:
             self.ATC_AUTH = json.loads(r.text)['value']
             return True
